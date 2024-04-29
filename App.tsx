2961e8018cdadc5ec522afdd6939053ed4223543
Luna <lunaarsilva@gmail.com>
2024-04-29 00:52:18 -0300

    First commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import SignIn from "./src/screens/SignIn";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/theme";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { Loading } from "./src/components/Loading";
+import { StatusBar } from "react-native";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (!fontsLoaded) {
+    return (<Loading />);
+  }
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <SignIn />
+    </ThemeProvider>
+  )
+}
